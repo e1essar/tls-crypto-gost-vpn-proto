df129ffa7572c98174fab39c3f7a69abe092c251
e1essar <igorvaliulin770@gmail.com>
2025-11-12 20:09:46 +0700

    Deleted old project

--- include/engine/EngineLoader.h
+++
@@ -1,9 +0,0 @@
-// tls-crypto-gost-vpn-proto-tls13\include\engine\EngineLoader.h
-#pragma once
-#include "IEngineLoader.h"
-
-namespace tls {
-class EngineLoader : public IEngineLoader {
-public:
-    ENGINE* loadEngine(const std::string& engineId) override;
-}
